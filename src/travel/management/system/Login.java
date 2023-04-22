9b03514c2f21ef87bd5ad4795d70d0dacb8f22c8
Spacenaut47 <sohammondal81@gmail.com>
2023-04-22 21:06:50 +0530

    first commit

---
+++ src/travel/management/system/Login.java
@@ -0,0 +1,90 @@
+package travel.management.system;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+public class Login extends JFrame {
+
+    Login() {
+        setSize(900, 400);
+        setLocation(350, 200);
+        setLayout(null);
+
+        getContentPane().setBackground(new Color(242, 243, 245));
+
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(32, 33, 36));
+        p1.setBounds(0, 0, 400, 400);
+        p1.setLayout(null);
+        add(p1);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/Login.png"));
+        Image i2 = i1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(100, 120, 200, 200);
+        p1.add(image);
+
+        JPanel p2 = new JPanel();
+        p2.setLayout(null);
+        p2.setBounds(400, 30, 450, 300);
+        add(p2);
+
+        // USERNAME LABEL AND TEXTBOX
+        JLabel username = new JLabel("Username:");
+        username.setBounds(60, 20, 100, 25);
+        username.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+        p2.add(username);
+
+        JTextField tusername = new JTextField();
+        tusername.setBounds(60, 60, 300, 30);
+        tusername.setBorder(BorderFactory.createEmptyBorder());
+        p2.add(tusername);
+
+        // PASSWORD LABEL AND TEXTBOX
+        JLabel password = new JLabel("Password:");
+        password.setBounds(60, 110, 100, 25);
+        password.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+        p2.add(password);
+
+        JPasswordField tpassword = new JPasswordField();
+        tpassword.setBounds(60, 150, 300, 30);
+        tpassword.setBorder(BorderFactory.createEmptyBorder());
+        p2.add(tpassword);
+
+        // BUTTONS
+        JButton login = new JButton("Login");
+        login.setBounds(60, 200, 130, 30);
+        login.setBackground(new Color(68, 189, 50));
+        login.setForeground(Color.WHITE);
+        login.setBorder(new LineBorder(new Color(68, 189, 50)));
+        p2.add(login);
+
+        JButton signup = new JButton("Signup");
+        signup.setBounds(200, 200, 130, 30);
+        signup.setBackground(new Color(36, 37, 42));
+        signup.setForeground(Color.WHITE);
+        signup.setBorder(new LineBorder(new Color(36, 37, 42)));
+        p2.add(signup);
+
+        JButton for_pass = new JButton("Forgot Password?");
+        for_pass.setBounds(130, 250, 170, 30);
+        for_pass.setBackground(new Color(36, 37, 42));
+        for_pass.setForeground(Color.WHITE);
+        for_pass.setBorder(new LineBorder(new Color(36, 37, 42)));
+        p2.add(for_pass);
+
+        JLabel text = new JLabel("Trouble logging in?");
+        text.setBounds(300, 250, 150, 20);
+        text.setForeground(new Color(68, 189, 50));
+        p2.add(text);
+
+        setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        new Login();
+    }
+
+}
---
+++ src/travel/management/system/Signup.java
@@ -0,0 +1,83 @@
+package travel.management.system;
+import javax.swing.*;
+import java.awt.*;
+
+public class Signup extends JFrame {
+    Signup(){
+        setBounds(150,200,900,360);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(133,193,233));
+        p1.setLayout(null);
+        p1.setBounds(0,0,500,400);
+        add(p1);
+        
+        //Username 
+        JLabel username = new JLabel("Username:");
+        username.setFont(new Font("Tahoma", Font.BOLD, 14));
+        username.setBounds(50,20,125,25);
+        p1.add(username); 
+        
+        JTextField tusername = new JTextField();
+        tusername.setBounds(190,20,180,25);
+        tusername.setBorder(BorderFactory.createEmptyBorder());
+        p1.add(tusername);
+        
+        
+        //Name
+        JLabel name = new JLabel("Name:");
+        name.setFont(new Font("Tahoma", Font.BOLD, 14));
+        name.setBounds(50,60,125,25);
+        p1.add(name); 
+        
+        JTextField tname = new JTextField();
+        tname.setBounds(190,60,180,25);
+        tname.setBorder(BorderFactory.createEmptyBorder());
+        p1.add(tname);
+        
+        //Password
+        JLabel password = new JLabel("Password:");
+        password.setFont(new Font("Tahoma", Font.BOLD, 14));
+        password.setBounds(50,100,125,25);
+        p1.add(password); 
+        
+        JPasswordField tpassword = new JPasswordField();
+        tpassword.setBounds(190,100,180,25);
+        tpassword.setBorder(BorderFactory.createEmptyBorder());
+        p1.add(tpassword);
+        setVisible(true);
+        
+        //Confirm Password
+        JLabel cpassword = new JLabel("Confirm Password:");
+        cpassword.setFont(new Font("Tahoma", Font.BOLD, 14));
+        cpassword.setBounds(50,140,150,25);
+        p1.add(cpassword); 
+        
+        JPasswordField tcpassword = new JPasswordField();
+        tcpassword.setBounds(190,140,180,25);
+        tcpassword.setBorder(BorderFactory.createEmptyBorder());
+        p1.add(tcpassword);
+        
+        //Security Question
+        JLabel security = new JLabel("Security Question: ");
+        security.setFont(new Font("Tahoma", Font.BOLD, 14));
+        security.setBounds(50,180,150,25);
+        p1.add(security);
+        
+        Choice tsecurity = new Choice();
+        tsecurity.add("What is your mother's maiden name?");
+        tsecurity.add("What is your High School's Name?");
+        tsecurity.add("In what city did you meet your spouse/significant other?");
+        tsecurity.add("What is the middle name of your youngest child?");
+        tsecurity.add("Where were you when you had your first kiss?");
+        tsecurity.add("What was the name of your first stuffed animal?");
+        
+        
+        setVisible(true);
+    }
+    public static void main(String args[]){
+        new Signup();
+    }
+}
